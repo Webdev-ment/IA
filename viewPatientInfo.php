4e134479fba685f760274c583babbb94f272e6ef
Webdev-ment <73551364+Webdev-ment@users.noreply.github.com>
2021-04-25 18:42:54 -0500

    Create viewPatientInfo.php

---
+++ viewPatientInfo.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create Patient Record</title>
+</head>
+<nav class="navbar navbar-expand-sm bg-warning navbar-dark sticky-top" >
+<ul class="navbar-nav" style="font-weight:bold;">
+    <li class="nav-item active">
+      <a class="nav-link" href="./index.php" >Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="./registration.php">Register</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Services</a>
+    </li>
+    <!-- <li class="nav-item">
+      <a class="nav-link disabled" href="#">Disabled</a>
+    </li> -->
+  </ul>
+
+</nav>
+
+<body class="patientbg" >
+<h1>Patient Record</h1>
+
+
+
+
+
+
+
+
+</body>
+<footer class="bg-warning">
+    <p>Group2</p>
+</footer>
+</html>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
