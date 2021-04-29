cccf5dfd7db5a859d9331961cdeb4a36f1cc6a12
Webdev-ment <73551364+Webdev-ment@users.noreply.github.com>
2021-04-28 19:45:00 -0500

    login and registration
    Completed login and registration

--- index.php
+++ index.php
@@ -1,46 +1,61 @@
 <html>
 <head>
 <title>Home</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<link rel="stylesheet" href="stylesheet.css">
 </head>
-<nav class="navbar navbar-expand-sm bg-warning navbar-dark sticky-top" >
+<nav class=" navi navbar navbar-expand-sm  sticky-top" >
+<div class="">
+    <a class="navbar-brand" href="./index.php">
+      <img src="./EMR_S.png" alt="" width="200px" height="50px">
+    </a>
+</div>
 <ul class="navbar-nav" style="font-weight:bold;">
     <li class="nav-item active">
-      <a class="nav-link" href="#" >Home</a>
+      <a class="nav-link" href="./index.php" >Home</a>
     </li>
-    <li class="nav-item">
+    <li class="nav-item active">
       <a class="nav-link" href="./registration.php">Register</a>
     </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Services</a>
-    </li>
-    <!-- <li class="nav-item">
-      <a class="nav-link disabled" href="#">Disabled</a>
-    </li> -->
+    <li class="nav-item dropdown active">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Patients
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="./AddPatientInfo"><b>Create Patient Records</b></a>
+          <a class="dropdown-item" href="./viewPatientInfo"><b>View Patient Records</b></a>
+          <a class="dropdown-item" href="#"><b>Edit Patient Record</b></a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#" style="color: red;"><b>Delete Patient Records</b></a>
+        </div>
+      </li>
+
   </ul>
+  
 
 </nav>
-<body class="body-bg bg-dark">
+
+<body class="body-bg ">
     <div class="p-d">  
-        <div class="card right" style="background-color:white;">
+        <div class="card right" style="background-color:white; width: 400px;">
             <div class="card-body">
                 <h1 class="card-title" style="text-align: center; color: #8d0404">Login Here!</h1>
                 <form action="./login.php" method="POST">
                     <div class="form-group" >
-                        <label for="exampleInputEmail1">Email</label>
-                        <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <label for="">Email</label>
+                        <input type="email" class="form-control" name="email" id="" aria-describedby="emailHelp">
                         
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" name="password" class="form-control" id="exampleInputPassword1">                     
+                        <label for="">Password</label>
+                        <input type="password" name="password" class="form-control" id="">                     
                     </div>  
-                    <div>
-                    <label for="exampleDataList" class="form-label">Occupation</label>
-                        <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search...">
+                    <div class="form-group">
+                        <label for="" class="form-label">Occupation</label>
+                        <input class="form-control" list="datalistOptions" id="" placeholder="Type to search...">
                         <datalist id="datalistOptions">
-                          <option name="user" value="Doctor" >
-                          <option name="user" value="Receptionist">
-                          
+                          <option type="radio" name="user" value="Doctor">
+                          <option type="radio" name="user" value="Receptionist">
                         </datalist>
                     </div>
 
@@ -48,16 +63,17 @@
                     <input type="radio" name="user" value="doctor" id=""> Doctor -->
                     <br>
                     <button type="submit" class="btn btn-warning">Login</button>
+                    
                 </form>
+               
   </div>
-
-
+  
 </body>
-<footer class="bg-warning">
+<!-- <footer class="bg-warning">
     <p>Group2</p>
-</footer>
+</footer> -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </html>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-<link rel="stylesheet" href="stylesheet.css">
-<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
--- registration.php
+++ registration.php
@@ -5,29 +5,47 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="stylesheet.css">
 </head>
-<nav class="navbar navbar-expand-sm bg-warning navbar-dark sticky-top" >
+<nav class=" navi navbar navbar-expand-sm  sticky-top" >
+<div class="">
+    <a class="navbar-brand" href="./index.php">
+      <img src="./EMR_S.png" alt="" width="200px" height="50px">
+    </a>
+</div>
 <ul class="navbar-nav" style="font-weight:bold;">
     <li class="nav-item active">
       <a class="nav-link" href="./index.php" >Home</a>
     </li>
-    <li class="nav-item">
+    <li class="nav-item active">
       <a class="nav-link" href="./registration.php">Register</a>
     </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Services</a>
-    </li>
-    <!-- <li class="nav-item">
-      <a class="nav-link disabled" href="#">Disabled</a>
-    </li> -->
+    <li class="nav-item dropdown active">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Patients
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="./AddPatientInfo"><b>Create Patient Records</b></a>
+          <a class="dropdown-item" href="./viewPatientInfo"><b>View Patient Records</b></a>
+          <a class="dropdown-item" href="#"><b>Edit Patient Record</b></a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#" style="color: red;"><b>Delete Patient Records</b></a>
+        </div>
+      </li>
+
   </ul>
+  
+
+</nav>
 
 </nav>
 <body class="bg-dark registerbg" >
 
-<form action="./register.php" method="POST">
+<form action="./register.php" method="POST" >
+
 <div class="py-3">  
-  <div class="container">
+<div class="container" style="width: 800px;">
     <h1>Register Here!</h1>
     <p>Please fill in this form to create an account.</p>
     <hr>
@@ -62,9 +80,9 @@
 
     <button type="submit" value="Sign Up" class="btn btn-warning registerbtn">Register</button>
   </div>
-
+<br>
   
-  <div class="container signin py-3">
+  <div class="container signin py-2">
     <p>Already have an account? <a href="./index.php">Sign in</a>.</p>
   </div>
 
@@ -75,11 +93,7 @@
 
    
 </body>
-<footer class="bg-warning">
-    <p>Group2</p>
-</footer>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </html>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-<link rel="stylesheet" href="stylesheet.css">
-<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
